a02f0eacaff7f2b7b14c615f26c09f3e7c79d3f7
9rosLove <yarik2300@gmail.com>
2024-06-18 09:40:02 +0300

    add write_to_worksheet func in utils.py

--- utils.py
+++ utils.py
@@ -1,10 +1,25 @@
 import gspread
+import pandas as pd
 from google.oauth2.service_account import Credentials
+from gspread import Worksheet
 
 
 def authenticate(scopes: list, credentials: str) -> gspread.Client:
     credentials = Credentials.from_service_account_file(
-        filename=credentials, scopes=scopes
+        filename=credentials,
+        scopes=scopes
     )
     client = gspread.authorize(credentials)
+
     return client
+
+
+def write_to_worksheet(
+        worksheet: Worksheet,
+        df: pd.DataFrame,
+        overwrite: bool = False
+) -> None:
+    if overwrite:
+        worksheet.clear()
+
+    worksheet.update([df.columns.values.tolist()] + df.values.tolist())
