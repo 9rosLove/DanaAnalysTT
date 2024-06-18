63574da481ec3797e642aa874fe64bf58b6eeda0
9rosLove <yarik2300@gmail.com>
2024-06-18 14:16:20 +0300

    implement concurrent dataFrame posting to spreadsheets

--- config.py
+++ config.py
@@ -4,9 +4,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+EMAIL = os.getenv("EMAIL")
 SHEET_ID = os.getenv("SHEET_ID")
 GOOGLE_APPLICATION_CREDENTIALS = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
 
 SCOPES = [
     "https://www.googleapis.com/auth/spreadsheets",
-]
+    "https://www.googleapis.com/auth/drive",
+    ]
--- utils.py
+++ utils.py
@@ -1,7 +1,6 @@
 import gspread
 import pandas as pd
 from google.oauth2.service_account import Credentials
-from gspread import Worksheet
 
 
 def authenticate(scopes: list, credentials: str) -> gspread.Client:
@@ -15,11 +14,37 @@ def authenticate(scopes: list, credentials: str) -> gspread.Client:
 
 
 def write_to_worksheet(
-        worksheet: Worksheet,
+        worksheet: gspread.Worksheet,
         df: pd.DataFrame,
         overwrite: bool = False
 ) -> None:
     if overwrite:
         worksheet.clear()
 
+    worksheet.update([df.columns.values.tolist()] + df.values.tolist())
+
+
+def create_spreadsheet(gclient: gspread.Client, title: str) -> gspread.Spreadsheet:
+    return gclient.create(title)
+
+
+def share_spreadsheet(spreadsheet: gspread.Spreadsheet, email: str):
+    spreadsheet.share(email, perm_type="user", role="writer")
+
+
+def create_worksheet(
+        spreadsheet: gspread.Spreadsheet,
+        title: str,
+        rows: int,
+        cols: int
+) -> gspread.Worksheet:
+    return spreadsheet.add_worksheet(title=title, rows=rows, cols=cols)
+
+
+def add_worksheet_and_write_data(spreadsheet, df, sheet_name) -> None:
+    worksheet = spreadsheet.add_worksheet(
+        title=sheet_name,
+        rows=df.shape[0] + 1,
+        cols=df.shape[1]
+    )
     worksheet.update([df.columns.values.tolist()] + df.values.tolist())
